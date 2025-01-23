ad0006f4cf5f25f35a9470e231c26a46e41320d7
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 14:11:25 +0100

    added character img

--- models/world.class.js
+++ models/world.class.js
@@ -5,9 +5,14 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    ctx;
 
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw(){
-
+        this.ctx.drawImage(this.character.img,this.character.x,this.character.y,100,100 );
     }
 }

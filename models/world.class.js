f95c748a6076ebf748baef824e95e0d7610439c0
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 22:10:53 +0100

    bg placed better

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,7 @@ class World{
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 80)
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0)
     ];
     canvas;
     ctx;
@@ -23,10 +23,11 @@ class World{
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.backgroundObjects);
+        
 
 
         let self = this;

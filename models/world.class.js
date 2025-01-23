2c0a64ee2c816e8c2b6d6118bb2889c0ae2e7833
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 21:51:06 +0100

    compressed code

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,9 @@ class World{
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png')
+    ];
     canvas;
     ctx;
 
@@ -20,16 +23,10 @@ class World{
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img,this.character.x,this.character.y, this.character.width,this.character.height);
-        this.enemies.forEach(enemy =>{
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
-
-
-
-        this.clouds.forEach(cloud =>{
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
 
 
         let self = this;
@@ -37,4 +34,15 @@ class World{
             self.draw()
         });
     }
+
+    addObjectsToMap(objects){
+        objects.forEach(o =>{
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+
 }

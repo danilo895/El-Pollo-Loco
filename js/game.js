63a55b429d4117fab20c9815c696ba7286284bdd
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 13:54:47 +0100

    added enemies and charakters img paths

--- js/game.js
+++ js/game.js
@@ -5,7 +5,8 @@ let world = new World();
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    
+
     console.log('My Charakter is', world.character);
+    console.log('My enemies are', world.enemies)
 
 }

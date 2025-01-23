366283e86b777a91dbf354fb0c0e0b75f4e2c103
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 14:40:42 +0100

    static moving character

--- models/world.class.js
+++ models/world.class.js
@@ -5,14 +5,21 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.ctx.drawImage(this.character.img,this.character.x,this.character.y, this.character.height,this.character.width);
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw()
+        });
     }
 }

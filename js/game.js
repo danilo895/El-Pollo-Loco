2c5ee90b467601f13c41d0d4f5cc9c164728e9db
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-23 13:12:08 +0100

    basics

--- js/game.js
+++ js/game.js
@@ -1,16 +1,11 @@
 let canvas;
 let ctx;
-let character = new Character();
-let enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
+let world = new World();
 
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-
-    console.log('My Charakter is', character)
+    
+    console.log('My Charakter is', world.character);
 
 }

30b8e4fd024fb9025c53c785f8cc44599a165ae5
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-24 12:16:59 +0100

    character moving back and forth

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject{
     height = 280;
     y = 155;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -20,14 +21,25 @@ class Character extends MovableObject{
 
 
     animate(){
-        setInterval(() =>{
+
+        setInterval(() => {
             if(this.world.keyboard.RIGHT){
+                this.x += this.speed;
+            }
+            if(this.world.keyboard.LEFT){
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
+        setInterval(() =>{
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
-    }, 100);
+    }, 50);
     
     }
 

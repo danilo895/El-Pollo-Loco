76ba15086d21476fb7192fd63943e03e0b1c82a8
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-26 23:06:56 +0100

    added instanceof frame

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -36,11 +36,13 @@ class MovableObject{
     }
 
     drawFrame(ctx){
+        if(this instanceof Character || this instanceof Chicken){
         ctx.beginPath();
         ctx.lineWidth = '5';
         ctx.strokeStyle = 'blue';
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.stroke();
+    }
     }
 
 

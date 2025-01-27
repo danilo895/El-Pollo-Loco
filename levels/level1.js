b0812b8934f2c096d5d068286593a405bb6bf740
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-27 21:51:46 +0100

    inserted some chicks

--- levels/level1.js
+++ levels/level1.js
@@ -1,8 +1,11 @@
 const level1 = new Level(
     [
         new Chicken(),
+        new Chick(),
         new Chicken(),
         new Chicken(),
+        new Chick(),
+        new Chick(),
         new Endboss()
     ],
     [
---
+++ models/chick.class.js
@@ -0,0 +1,30 @@
+class Chick extends MovableObject{
+    y = 390;
+    height = 50;
+    width = 80;
+    offsetX = 0;   // Offset für den Gegner
+    offsetY = 0;   // Offset für den Gegner
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+    constructor(){
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 700;
+        this.speed = 0.15 + Math.random() * 0.2;
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+        setInterval(() =>{
+            this.playAnimation(this.IMAGES_WALKING);
+    },200);
+    }
+}

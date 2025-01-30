731ede54a00a86df4073209c7e77c1512cd1198a
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-30 11:19:27 +0100

    attackanimation bosschicken works

--- models/boss-chicken.class.js
+++ models/boss-chicken.class.js
@@ -23,103 +23,77 @@ class BossChicken extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
 
-IMAGES_ATTACKING = [
-    'img/4_enemie_boss_chicken/3_attack/G13.png',
-    'img/4_enemie_boss_chicken/3_attack/G14.png',
-    'img/4_enemie_boss_chicken/3_attack/G15.png',
-    'img/4_enemie_boss_chicken/3_attack/G16.png',
-    'img/4_enemie_boss_chicken/3_attack/G17.png',
-    'img/4_enemie_boss_chicken/3_attack/G18.png',
-    'img/4_enemie_boss_chicken/3_attack/G19.png',
-    'img/4_enemie_boss_chicken/3_attack/G20.png'
-
-];
-
-
+    IMAGES_ATTACKING = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
+    ];
 
     constructor() {
         super().loadImage('img/4_enemie_boss_chicken/2_alert/G5.png');
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_ALERT);
         this.loadImages(this.IMAGES_ATTACKING);
-        this.x = 600;
+        this.x = 2000;
         this.speed = 0.15 + Math.random() * 0.5;
-        //this.animateWalking();
-        //this.animateAlert();
-        this.animateAttack();
+
+        this.startAttackCycle(); // Neuer Zyklus für Angriff und Rücklauf starten
     }
+
     setWorld(world) {
         this.world = world;
     }
-    
 
-    animateWalking(){
-        setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60);
+    startAttackCycle() {
+        let startX = this.x; // Ursprungsposition merken
 
-        setInterval(() =>{
-            this.playAnimation(this.IMAGES_WALKING);
-    },200);
+        const attackLoop = () => {
+            this.animateAttack(() => {
+                this.moveBackToStart(startX, attackLoop); // Starte den Rückweg, dann wiederhole Angriff
+            });
+        };
+
+        attackLoop(); // Erster Angriff starten
     }
 
-    animateAlert(){
-        setInterval(() =>{
-            this.playAnimation(this.IMAGES_ALERT);
-    },200);
-    }
+    animateAttack(callback) {
+        let originalSpeed = this.speed; 
+        this.speed = 12; 
+        let attackInterval = setInterval(() => this.moveLeft(), 1000 / 60);
+        let animationInterval = setInterval(() => this.playAnimation(this.IMAGES_ATTACKING), 200);
 
-    animateAttack() {
-        let originalSpeed = this.speed; // Originalgeschwindigkeit speichern
-        this.speed = 12; // Geschwindigkeit für den Angriff erhöhen
-        let startX = this.x; // Ursprungsposition speichern
-    
-        let attackInterval = setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60);
-    
-        let animationInterval = setInterval(() => {
-            this.playAnimation(this.IMAGES_ATTACKING);
-        }, 200);
-    
-        // Nach 800ms normale Geschwindigkeit & Richtung wechseln
         setTimeout(() => {
-            clearInterval(attackInterval); // Stoppt den Angriff nach 800ms
-            clearInterval(animationInterval); // Stoppt die Angriffsanimation
+            clearInterval(attackInterval);
+            clearInterval(animationInterval);
             this.speed = originalSpeed;
-            this.moveBackToStart(startX);
+            if (callback) callback(); // Starte den Rückweg, falls Callback vorhanden
         }, 800);
     }
-    
-    
-    moveBackToStart(startX) {
+
+    moveBackToStart(startX, callback) {
         let returnInterval = setInterval(() => {
             if (this.x < startX) {
-                this.moveRight(); // Bewege BossChicken nach rechts
+                this.moveRight();
             } else {
-                clearInterval(returnInterval); // Stoppe Bewegung, wenn Startpunkt erreicht
-                this.animateWalking(); // Nach dem Rückweg wieder normale Bewegung
+                clearInterval(returnInterval);
+                if (callback) callback(); // Starte neuen Angriff
             }
         }, 1000 / 60);
     
-        // Animation langsamer abspielen (alle 200ms, wie normale Laufanimation)
-        let animationInterval = setInterval(() => {
-            this.playAnimation([...this.IMAGES_WALKING].reverse()); // Rückwärtslauf
-        }, 200); // Langsamer abspielen
+        // Rückwärtslauf-Animation langsamer abspielen (300ms statt 200ms)
+        let reversedImages = [...this.IMAGES_WALKING].reverse();
+        let frameIndex = 0;
     
-        // Animation stoppen, wenn Bewegung vorbei ist
-        setTimeout(() => {
-            clearInterval(animationInterval);
-        }, Math.abs(this.x - startX) / this.speed * 60); // Berechnet ungefähre Laufzeit
+        let walkingAnimation = setInterval(() => {
+            this.img = this.imageCache[reversedImages[frameIndex]];
+            frameIndex = (frameIndex + 1) % reversedImages.length; // Nächstes Bild
+        }, 300); // Langsamer als normales Laufen
+    
+        setTimeout(() => clearInterval(walkingAnimation), Math.abs(this.x - startX) / this.speed * 60);
     }
-    
-    
-    
-    
-    
-
-
-
-    
 }
-

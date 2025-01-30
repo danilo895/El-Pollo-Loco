4fddf992231721b83ed5768ce64a4ffb427fa6b5
Daniel Loeffler <loefflerdaniel@web.de>
2025-01-30 13:59:42 +0100

    bottle located tracking

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,18 +1,31 @@
-class ThrowableObject extends MovableObject{
-    constructor(x, y){
+class ThrowableObject extends MovableObject {
+    constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.x = x;
         this.y = y;
         this.height = 90;
         this.width = 70;
         this.throw();
+        this.trackPosition(); // ✅ Starte das Tracking der Position
     }
 
-    throw(){
+    throw() {
         this.speedY = 30;
         this.applyGravity();
-        setInterval(() =>{
+        setInterval(() => {
             this.x += 10;
         }, 25);
+    }
+
+    trackPosition() {
+        let trackInterval = setInterval(() => {
+            console.log(`🚀 Flasche Position: x=${this.x}, y=${this.y}`);
+
+            // ❌ Stoppe das Tracking, wenn die Flasche den Bildschirm verlässt
+            if (this.y > 500 || this.x < 0 || this.x > 4000) { 
+                clearInterval(trackInterval);
+                console.log("🛑 Tracking gestoppt: Flasche ist aus dem Bildschirm!");
+            }
+        }, 20); // ✅ Alle 20ms wird die Position geloggt
     }
 }

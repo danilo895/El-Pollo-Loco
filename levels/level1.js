20909847f2b0444fef87f1b26de547b29304c63c
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-05 22:05:22 +0100

    setenemies in level

--- levels/level1.js
+++ levels/level1.js
@@ -1,3 +1,4 @@
+let enemiesSetted = false;
 let level1 = new Level(
     [
         //new Chicken(),
@@ -60,3 +61,48 @@ let level1 = new Level(
         new TabascoBottle(),
     ],
 ); 
+
+
+
+
+function setEnemies() {
+    document.getElementById('overlay-start-game').classList.add('d-none');
+    document.getElementById('canvas').classList.remove('d-none');
+    if (enemiesSetted) {
+        return;
+    }
+    level1.enemies = [
+        new Chicken(),
+        //new Chick(),
+        //new Chicken(),
+        //new Chicken(),
+        //new Chicken(),
+        //new Chick(),
+        new BossChicken()
+    ];
+    enemiesSetted = true;
+    world.setWorld(); 
+    world.draw();
+}
+
+
+function setEnemiesAfterLose() {
+    document.getElementById('overlay-losing-screen').classList.remove('d-flex');
+    document.getElementById('overlay-losing-screen').classList.add('d-none');
+    document.getElementById('canvas').classList.remove('d-none');
+    if (enemiesSetted) {
+        return;
+    }
+    level1.enemies = [
+        new Chicken(),
+        //new Chick(),
+        //new Chicken(),
+        //new Chicken(),
+        //new Chicken(),
+        //new Chick(),
+        new BossChicken()
+    ];
+    enemiesSetted = true;
+    world.setWorld(); 
+    world.draw();
+}

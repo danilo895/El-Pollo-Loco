4d726b69d13756f9428339d3242c01cc9af3213b
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-10 14:06:31 +0100

    improved coin xy

--- models/coin.class.js
+++ models/coin.class.js
@@ -15,7 +15,7 @@ class Coin extends DrawableObject {
     constructor() {
         super().loadImage('img/8_coin/coin_1.png');
         this.x = 350 + Math.random() * (1600 - 100);
-        this.y = 220 + Math.random() * (350 - 220);
+        this.y = 220 + Math.random() * (400 - 220);
         this.animate();
 
     }
